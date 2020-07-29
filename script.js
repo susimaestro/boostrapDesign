968f314d3d1d11d450af0122cbc54feba16e79ae
susimaestro <susanneaubert@gmail.com>
2020-07-30 00:12:42 +0100

    card hovers fixed!

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-
-
-
-
-
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});(function(){
-
-    /*SCROLL HOME*/
-sr.reveal('.hi',{}); 
-sr.reveal('.filters',{delay: 200}); 
-sr.reveal('.projects',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+(function(){
 
     'use strict';
 
